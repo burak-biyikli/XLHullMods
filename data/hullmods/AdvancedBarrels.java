6df3a5dcfb4b0d472d4a129fe7aecb5a661a401e
burak-biyikli <burak.biyikli@yahoo.com>
2024-12-21 19:50:41 -0600

    Advanced barrels now has flux instead of dps penalty

--- data/hullmods/AdvancedBarrels.java
+++ data/hullmods/AdvancedBarrels.java
@@ -10,21 +10,21 @@ public class AdvancedBarrels extends BaseHullMod {
 
 	public static final float BALLISTIC_RANGE_BONUS = 200f;
 	public static final float BALLISTIC_SPEED_BONUS = 25f;
-	public static final float ROF_PENALTY = 10f;
-	public static final float TURN_PENALTY = 10f;
+	public static final float BALLISTIC_FLUX_PENALTY = 10f;
+	public static final float BALLISTIC_TURN_PENALTY = 10f;
 	
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
 		stats.getBallisticWeaponRangeBonus().modifyFlat(id, BALLISTIC_RANGE_BONUS);
 		stats.getBallisticProjectileSpeedMult().modifyMult(id, 1f + BALLISTIC_SPEED_BONUS * 0.01f);
-		stats.getBallisticRoFMult().modifyMult(id, 1f - ROF_PENALTY * 0.01f);
-		stats.getWeaponTurnRateBonus().modifyPercent(id, -TURN_PENALTY);
+		stats.getBallisticWeaponFluxCostMod().modifyMult(id, 1f + BALLISTIC_FLUX_PENALTY * 0.01f);
+		stats.getWeaponTurnRateBonus().modifyPercent(id, -BALLISTIC_TURN_PENALTY);
 	}
 	
 	public String getDescriptionParam(int index, HullSize hullSize) {
 		if (index == 0) return "" + (int) BALLISTIC_RANGE_BONUS;
 		if (index == 1) return "" + (int) BALLISTIC_SPEED_BONUS + "%";
-		if (index == 2) return "" + (int) ROF_PENALTY + "%";
-		if (index == 3) return "" + (int) TURN_PENALTY + "%";
+		if (index == 2) return "" + (int) BALLISTIC_FLUX_PENALTY + "%";
+		if (index == 3) return "" + (int) BALLISTIC_TURN_PENALTY + "%";
 		return null;
 	}
 }
